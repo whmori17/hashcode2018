40773c9956bf85671f6182c75403198fbe0d3ab7
Dirk39 <andreaquin@alice.it>
2018-03-01 21:21:11 +0100

    first attempt

--- example.php
+++ example.php
@@ -6,7 +6,7 @@ $map = new \Map('test.in');
 
 $rides = $map->map;
 
-$vehicles_results = array_fill(0, $map->F, ['n_rides' => 0 ] );
+$vehicles_results = array_fill(0, $map->F, ['n_rides' => 0 ,'rides' => []] );
 $vehicles_riding = array_fill(0, $map->F, null );
 $terminated_rides = [];
 
@@ -25,8 +25,8 @@ function count_free_vehicles($riding_vehicles) {
 }
 
 function search_ride_assigned($ride, $vehicles_riding) {
-  foreach($vehicles_riding as  $vr) {
-    if($vr === $ride) return true;
+  foreach($vehicles_riding as $vh => $vr) {
+    if($vr[0] === $ride) return $vh;
   }
 
   return false;
@@ -39,17 +39,22 @@ for($i = 0; $i < $map->T; $i++ ) {
     //se la corsa è già stata fatta -> next
     if(in_array($ride, $terminated_rides)) {continue;}
     //se la corsa non è stata assegnata
-    if(false === (search_ride_assigned($ride, $vehicles_riding)))
+    if(false === ($vh = search_ride_assigned($ride, $vehicles_riding)))
     {
       // non ci sono auto disponibili
       if(!count_free_vehicles($vehicles_riding)) continue;
-      $vehicle_avbl = get_free_vehicle($vehicles_riding);
-      $vehicles_riding[$vehicle_avbl] = $ride;
+      $vehicle_avbl = $vh = get_free_vehicle($vehicles_riding);
+      $vehicles_riding[$vehicle_avbl] = [$ride, $map->calculateDistance($ride) + $rides[$ride][4]];
     }
 
+    $vehicles_riding[$vh][1]--;
+    if($vehicles_riding[$vh][1] === 0) {
+      $vehicles_results[$vh]['n_rides']++;
+      $vehicles_results[$vh]['rides'][] = $ride;
+      $terminated_rides[] = $ride;
+      unset($vehicles_riding[$vh]);
+    }
   }
-
-  break;
 }
 
-var_dump($vehicles_riding);
+var_dump($vehicles_results);
