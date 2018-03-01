4462e54d47759ad4d1010e28d7beb135a5a3557d
Dirk39 <andreaquin@alice.it>
2018-03-01 20:21:59 +0100

    rewritted Map function

--- Map.php
+++ Map.php
@@ -8,7 +8,7 @@ class Map
     private $N;
     private $B;
     private $T;
-    private $map;
+    public $map;
 
     /**
      * Map constructor.
@@ -16,28 +16,25 @@ class Map
     public function __construct($nameFile)
     {
         $file = explode("\n", file_get_contents($nameFile));
-
-        $params = explode(' ', $file[0]);
-        $this->R = $params[0];
-        $this->C = $params[1];
-        $this->F = $params[2];
-        $this->N = $params[3];
-        $this->B = $params[4];
-        $this->T = $params[5];
-
-        $this->map = $this->getMapArray($file, $this->R);
-        $this->printMap();
-    }
-
-    private function getMapArray(Array $file, int $R) {
-        $map = [];
-
-        for ($i = 1; $i < $R+1; $i++) {
-            $map[$i-1] = str_split($file[$i]);
+        $this->map = [];
+        foreach ($file as $i => $line)
+        {
+          $params = explode(' ', $line);
+          if($i === 0) {
+            $this->R = $params[0];
+            $this->C = $params[1];
+            $this->F = $params[2];
+            $this->N = $params[3];
+            $this->B = $params[4];
+            $this->T = $params[5];
+          }
+          else {
+            $this->map[] = $params;
+          }
         }
-
-        return $map;
     }
+
+
 
     private function printMap() {
         echo 'R ' . $this->R . '&nbsp<br><br>';
